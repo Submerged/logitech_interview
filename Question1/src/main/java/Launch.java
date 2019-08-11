435c4dd4aa5090c83faf152eebcdb99e78038ac7
Derek Leblanc <leblanc@curingresin.com>
2019-08-11 12:32:46 -0300

    Starting with a pretty bad approach to this question just to get my feet wet. I know this isn't optimal, but it was quick and gives me a starting point and let's me validate  some unit tests. I know dynamic programming is the best way to do this, but it's been quite some time since I've used it.

--- Question1/src/main/java/Launch.java
+++ Question1/src/main/java/Launch.java
@@ -1,3 +1,7 @@
+
+import java.util.Arrays;
+import java.util.Vector;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +14,10 @@
  */
 public class Launch {
     public static void main(String args[]) {
+        RobbersProblem robbersProblem = new RobbersProblem();
+        Vector<Integer> housesAndValues = new Vector<>(Arrays.asList(1,2,3,1,10,80,80,1000));
         
+        int maxTheft = robbersProblem.maximizeTheft(housesAndValues);
+        System.out.println(String.format("Max Theft %d", maxTheft));
     }
 }
--- Question1/src/main/java/RobbersProblem.java
+++ Question1/src/main/java/RobbersProblem.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Vector;
 
 /*
@@ -21,6 +23,33 @@ public class RobbersProblem {
      * @return the maximum amount we can rob without alerting the police
      */
     public int maximizeTheft(Vector<Integer> housesAndValues) {
-        return 0;
+        int n = housesAndValues.size();
+        ArrayList<Integer> validTotalCashedRobbedSubsets = new ArrayList<>();
+        
+        for(int i = 0; i < (1 << n); i++) {
+            ArrayList<Integer> housesInSet = new ArrayList<>();
+            int totalCashRobbed = 0;
+            for(int j = 0; j < n; j++) {
+                if((i & (1 << j)) > 0) {
+                    housesInSet.add(j);
+                    totalCashRobbed += housesAndValues.get(j);
+                }
+            }
+            if(validateNonAdjacentHouses(housesInSet)){
+                validTotalCashedRobbedSubsets.add(totalCashRobbed);
+            }
+        }
+        return Collections.max(validTotalCashedRobbedSubsets);
+    }
+    
+    public boolean validateNonAdjacentHouses(ArrayList<Integer> housesRobbed) {
+        for(int i = 0; i < housesRobbed.size();i++){
+            if(i > 0){
+                if(housesRobbed.get(i)-housesRobbed.get(i-1) <= 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
